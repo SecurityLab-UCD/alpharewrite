f2fdbd4d68d3843dcbf56bd7c4b4d3310b5d3fef
Yifeng He <yfhe.prsn@gmail.com>
2024-12-25 21:40:34 -0800

    fixL format with ormolu

--- app/Main.hs
+++ app/Main.hs
@@ -2,19 +2,19 @@
 
 module Main where
 
-import FunctionRewrite (renameFunctions)
-import Task( Task(..) )
 import Data.Aeson (decode, encode)
 import qualified Data.ByteString.Lazy as BL
+import FunctionRewrite (renameFunctions)
 import System.Environment (getArgs)
+import Task (Task (..))
 
 main :: IO ()
 main = do
   args <- getArgs
   input <- case args of
-             []      -> BL.getContents
-             [file]  -> BL.readFile file
-             _       -> error "Usage: alpharewrite [tasks.json]"
+    [] -> BL.getContents
+    [file] -> BL.readFile file
+    _ -> error "Usage: alpharewrite [tasks.json]"
 
   -- We expect an array of tasks in the input JSON
   case decode input :: Maybe [Task] of
--- src/FunctionRewrite.hs
+++ src/FunctionRewrite.hs
@@ -2,19 +2,27 @@
 
 module FunctionRewrite (renameFunctions) where
 
-import Language.Haskell.Exts
-  ( -- Parsing
-    ParseMode(..), ParseResult(..), defaultParseMode, parseDeclWithMode
-  , Extension(..), KnownExtension(..)
-    -- AST Types
-  , Decl(..), Name(..), SrcSpanInfo
-  , prettyPrint
-  )
-import qualified Data.Text as T
-import Data.List (foldl')
+-- Parsing
+
+-- AST Types
+
 import Control.Monad (forM)
 import Data.Generics (everywhere, mkT)
-import Task (Task(..))
+import Data.List (foldl')
+import qualified Data.Text as T
+import Language.Haskell.Exts
+  ( Decl (..),
+    Extension (..),
+    KnownExtension (..),
+    Name (..),
+    ParseMode (..),
+    ParseResult (..),
+    SrcSpanInfo,
+    defaultParseMode,
+    parseDeclWithMode,
+    prettyPrint,
+  )
+import Task (Task (..))
 
 --------------------------------------------------------------------------------
 -- 1) Decl type alias for convenience
@@ -28,20 +36,23 @@ type MyDecl = Decl SrcSpanInfo
 parseOneDecl :: String -> Either String MyDecl
 parseOneDecl src =
   case parseDeclWithMode defaultMode src of
-    ParseOk d        -> Right d
+    ParseOk d -> Right d
     ParseFailed _ err -> Left ("Parse error: " ++ err)
   where
     defaultMode :: ParseMode
-    defaultMode = defaultParseMode
-      { extensions = map EnableExtension
-          [ MultiParamTypeClasses
-          , FlexibleContexts
-          , FlexibleInstances
-          -- Further work: support more type extensions
-          -- , GADTs
-          -- , TypeFamilies
-          ]
-      }
+    defaultMode =
+      defaultParseMode
+        { extensions =
+            map
+              EnableExtension
+              [ MultiParamTypeClasses,
+                FlexibleContexts,
+                FlexibleInstances
+                -- Further work: support more type extensions
+                -- , GADTs
+                -- , TypeFamilies
+              ]
+        }
 
 --------------------------------------------------------------------------------
 -- 3) Convert a Decl back to string
@@ -63,13 +74,14 @@ extractNamesFromTypeSig src = do
   case decl of
     TypeSig _ names _ -> Right (map nameToString names)
     _ -> Right []
-    -- Left ("Not a type signature: " ++ src)
+
+-- Left ("Not a type signature: " ++ src)
 
 -- Convert Name (with annotation) to a user-friendly string:
 --   Ident "not"   -> "not"
 --   Symbol "=="   -> "(==)"
 nameToString :: Name SrcSpanInfo -> String
-nameToString (Ident  _ s) = s
+nameToString (Ident _ s) = s
 nameToString (Symbol _ s) = "(" ++ s ++ ")"
 
 --------------------------------------------------------------------------------
@@ -84,15 +96,14 @@ renameName old new = everywhere (mkT goName)
     goName :: Name SrcSpanInfo -> Name SrcSpanInfo
     goName (Ident l s)
       -- If the name is exactly 'old', rename it
-      | s == old             = Ident l new
+      | s == old = Ident l new
       -- Sometimes the 'old' might be e.g. "(==)", so compare with parentheses removed
       | "(" ++ s ++ ")" == old = Ident l new
-      | otherwise            = Ident l s
-
+      | otherwise = Ident l s
     goName (Symbol l s)
-      | s == old             = Symbol l new
+      | s == old = Symbol l new
       | "(" ++ s ++ ")" == old = Symbol l new
-      | otherwise            = Symbol l s
+      | otherwise = Symbol l s
 
 -- | Apply multiple renamings in sequence.
 renameAll :: [(String, String)] -> MyDecl -> MyDecl
@@ -122,15 +133,15 @@ renameFunctions t = do
     extractNamesFromTypeSig (T.unpack depStr)
 
   let allNames = sigNames ++ concat depNamesList
-      renameMap = zip allNames [ "f" ++ show i | i <- [1..] ]
-       -- e.g. [("(==)","f1"),("not","f2"),("(/=)","f3"),...]
+      renameMap = zip allNames ["f" ++ show i | i <- [1 ..]]
+  -- e.g. [("(==)","f1"),("not","f2"),("(/=)","f3"),...]
 
   ----------------------------------------------------------------
   -- (B) Parse & rename the signature
   ----------------------------------------------------------------
   sigDecl <- parseOneDecl (T.unpack $ signature t)
-  let sigRen  = renameAll renameMap sigDecl
-      newSig  = declToString sigRen
+  let sigRen = renameAll renameMap sigDecl
+      newSig = declToString sigRen
 
   ----------------------------------------------------------------
   -- (C) Parse & rename each dependency
@@ -150,8 +161,9 @@ renameFunctions t = do
   ----------------------------------------------------------------
   -- (E) Return updated Task
   ----------------------------------------------------------------
-  pure t
-    { signature    = T.pack newSig
-    , dependencies = map T.pack newDeps
-    , code         = T.pack newCode
-    }
+  pure
+    t
+      { signature = T.pack newSig,
+        dependencies = map T.pack newDeps,
+        code = T.pack newCode
+      }
