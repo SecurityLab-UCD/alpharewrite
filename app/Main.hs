724991dea3f5e4c13d171d8aa383252630b8ec1f
Yifeng He <yfhe.prsn@gmail.com>
2024-12-26 22:45:15 -0800

    feat: option to select what to rewrite

--- app/Main.hs
+++ app/Main.hs
@@ -11,20 +11,34 @@ import System.Environment (getArgs)
 import Task (Task (..))
 import Data.Either (rights)
 
+rewriteTypes :: Task -> Either String Task
+rewriteTypes = rewriteTypeVars >> rewriteAtomicTypes
+
 alphaRewrite :: Task -> Either String Task
-alphaRewrite task = renameFunctions task >>= rewriteTypeVars >>= rewriteAtomicTypes
+alphaRewrite = renameFunctions >> rewriteTypes
 
 main :: IO ()
 main = do
   args <- getArgs
-  input <- case args of
-    [] -> BL.getContents
-    [file] -> BL.readFile file
-    _ -> error "Usage: alpharewrite [tasks.json]"
+  (func, input) <- case args of
+    [] -> error "Usage: alpharewrite [0|1|2] [tasks.json]"
+    [opt] -> do
+      input <- BL.getContents
+      return (selectFunction opt, input)
+    [opt, file] -> do
+      input <- BL.readFile file
+      return (selectFunction opt, input)
+    _ -> error "Usage: alpharewrite [0|1|2] [tasks.json]"
 
   -- We expect an array of tasks in the input JSON
   case decode input :: Maybe [Task] of
     Nothing -> error "Could not parse JSON."
     Just tasks -> do
-      let renamed = rights (map alphaRewrite tasks)
+      let renamed = rights (map func tasks)
       BL.putStr (encode renamed)
+
+selectFunction :: String -> (Task -> Either String Task)
+selectFunction "0" = alphaRewrite
+selectFunction "1" = rewriteTypes
+selectFunction "2" = renameFunctions
+selectFunction _   = error "Invalid option. Use 0, 1, or 2."
